ac0b991bb5f01b5a55bce4632af94459efcfa136
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-24 23:08:43 -0400

    added header to index.js

---
+++ employee-directory/src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function Header () {
+    <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Use "Search" to filter results. Click the arrows on the headers to sort.</p>
+    </div>
+}
