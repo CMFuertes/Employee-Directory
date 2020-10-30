5bd7af5ceb9c23c69792131d0cdc63bed4e9c2da
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-30 18:10:18 -0400

    updated index.js

--- employee-directory/src/components/TableRow/index.js
+++ employee-directory/src/components/TableRow/index.js
@@ -2,3 +2,41 @@ import React, { useContext } from "react";
 import TableBody from "../TableBody";
 import "./style.css";
 import DataHook from "../../utils/DataContext";
+
+const TableRow = () => {
+    const context = useContext(DataHook);
+
+    return (
+
+        <div className="datatable mt-5">
+            <table
+                id="table"
+                className="table table-striped table-hover table-condensed"
+            >
+                <thead>
+                    <tr>
+                        {context.developerState.headings.map(({ name, width }) => {
+                            return (
+                                <th
+                                    className="col"
+                                    key={name}
+                                    style={{ width }}
+                                    onClick={() => {
+                                        context.handleSort(name.toLowerCase());
+                                    }}
+                                >
+                                    {name}
+                                    <span className="pointer"></span>
+                                </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+
+                <TableBody />
+            </table>
+        </div>
+    );
+}
+
+export default TableRow;
