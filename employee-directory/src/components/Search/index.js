168c33e2a6e2e3de9e109fbdef464dfa69a05214
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-10-30 18:08:10 -0400

    added index.js to search

---
+++ employee-directory/src/components/Search/index.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import DataContext from "../../utils/DataContext";
+
+const Search = () => {
+    const context = useContext(DataContext);
+return (
+    <div className="searchbox">
+      <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="">
+              Search
+            </span>
+          </div>
+          <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="name"
+          aria-label="Search"
+          onChange={e => context.handleSearchChange(e)}
+        />
+        </div>
+    </div>
+  );
+}
+export default Search;
