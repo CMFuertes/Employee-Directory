ca778f775490b3d958a60f87aed7f0ce09c0de87
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-11-02 19:33:22 -0500

    added css changes

---
+++ build/precache-manifest.956a4bc4eb31d8031bafa790d450f558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e6196b26b2556d891f9b61cccf1154",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "9cb1f3005e6147571b9c",
+    "url": "/employee-directory/static/css/main.86d68815.chunk.css"
+  },
+  {
+    "revision": "ee2f26d01d58828acbfa",
+    "url": "/employee-directory/static/js/2.74783cb9.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/employee-directory/static/js/2.74783cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb1f3005e6147571b9c",
+    "url": "/employee-directory/static/js/main.f7081bc3.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
